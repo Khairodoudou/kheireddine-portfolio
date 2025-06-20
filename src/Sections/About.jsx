cd7e12b30970258af4a7509aea896d4e197ec0dc
Khairodoudou <annabikhairo@gmail.com>
2025-06-20 18:08:46 +0100

    cv reglageee

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -71,10 +71,10 @@ function About() {
 
         <div className="pt-12 bg-gray-50 text-center flex justify-center items-center">
           <a
-            href="/kheireddine_debz_CV.pdf" 
-            download 
-            aria-label="Download CV"
-            className="relative group cursor-pointer text-sky-50 overflow-hidden h-16 w-64 rounded-md bg-sky-800 p-2 flex justify-center items-center font-extrabold"
+              href="/kheireddine_debz_CV.pdf"
+              download="kheireddine_debz_CV.pdf"
+              aria-label="Download CV"
+              className="relative group cursor-pointer text-sky-50 overflow-hidden h-16 w-64 rounded-md bg-sky-800 p-2 flex justify-center items-center font-extrabold"
           >
             <div className="absolute top-3 right-20 group-hover:top-12 group-hover:-right-12 z-10 w-40 h-40 rounded-full group-hover:scale-150 group-hover:opacity-50 duration-500 bg-sky-900"></div>
             <div className="absolute top-3 right-20 group-hover:top-12 group-hover:-right-12 z-10 w-32 h-32 rounded-full group-hover:scale-150 group-hover:opacity-50 duration-500 bg-sky-800"></div>
